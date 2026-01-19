6f4014335631a047cc2980a8216428f54f480a4e
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2026-01-19 20:47:50 -0300

    feat: add PomodoroConfig to fixed cors problems

--- src/main/java/com/juan/performanceApp/pomodoro/infrastructure/config/PomodoroConfig.java
+++ src/main/java/com/juan/performanceApp/pomodoro/infrastructure/config/PomodoroConfig.java
@@ -1,12 +1,26 @@
 package com.juan.performanceApp.pomodoro.infrastructure.config;
 
 
-//@Configuration
-//public final class PomodoroConfig {
-////
-////    @Bean
-////    public PomodoroRepositoryI findAllPomodoroRepository (){
-////        return new PomodoroRepositoryImpl() {};
-////
-////    }
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class PomodoroConfig  implements WebMvcConfigurer {
+//
+//    @Bean
+//    public PomodoroRepositoryI findAllPomodoroRepository (){
+//        return new PomodoroRepositoryImpl() {};
+//
+//    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
